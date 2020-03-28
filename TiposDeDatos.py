8f81556813c42155bc0ee16b6cb569d73ffb991a
Edwin Barrera 0909-18-6221 <systemedbar@gmail.com>
2020-03-28 09:12:43 -0600

    first commit

---
+++ TiposDeDatos.py
@@ -0,0 +1,16 @@
+def PRODUCTOSUMA():
+    primernumero =int(input("INTRODUCE EL PRIMER NUMERO"))
+    segundonumero =int(input("INTRODUCE EL SEGUNDO NUMERO"))
+
+    resultado=0
+    for i in range(0,segundonumero):
+        resultado=resultado+primernumero
+
+
+PRODUCTOSUMA()
+def PRDUCTOF():
+    i=0
+    resultado=1
+    while i=="F":
+        numero= int(input)
+        resultado=resultado*numero
